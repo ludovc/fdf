4988e392026fa9e691d201123dbefacef7adee86
Ludovico Casu <lucasu@c1r6p11.42firenze.it>
2024-12-11 18:39:08 +0100

    wip

--- fdf.c
+++ fdf.c
@@ -2,13 +2,35 @@
 #include "minilibx-linux/mlx.h"
 #include "minilibx-linux/mlx_int.h"
 
-int		main()
-{
-	void	*qualcosa;
-	void	*qualcosaltro;
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
 
-	qualcosa = mlx_init();
-	qualcosaltro = mlx_new_window(qualcosa, 500, 500, "fdf");
-	mlx_loop(qualcosa);
-	//mlx_pixel_put(qualcosa, qualcosaltro, 10, 10, 0x00FF0000);
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
 }
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 500, 500, "Hello world!");
+	img.img = mlx_new_image(mlx, 500, 500);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	my_mlx_pixel_put(&img, 10, 10, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
+}
+
