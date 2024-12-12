228065ae1e8d3857e53c31977d341bc6406bcab3
Ludovico Casu <lucasu@c1r6p11.42firenze.it>
2024-12-12 23:56:01 +0100

    wip

--- fdf.c
+++ fdf.c
@@ -13,9 +13,39 @@ typedef struct	s_data {
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
+	int		byte_per_pixel;
+	int		vertical_offset;
+	int		horizontal_offset;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	byte_per_pixel = data->bits_per_pixel / 8;
+	vertical_offset = y * data->line_length;
+	horizontal_offset = x * byte_per_pixel;
+	dst = data->addr + (vertical_offset + horizontal_offset);
 	*(unsigned int*)dst = color;
+}
+
+void	put_horizontal_line(t_data *img, int x, int y, int size)
+{
+	int		i;
+
+	i = 0;
+	while (i < size)
+	{
+		my_mlx_pixel_put(img, x + i, y, 0x00FFFFFF);
+		i++;
+	}
+}
+
+void	put_vertical_line(t_data *img, int x, int y, int size)
+{
+	int		i;
+
+	i = 0;
+	while (i < size)
+	{
+		my_mlx_pixel_put(img, x, y + i, 0x00FFFFFF);
+		i++;
+	}
 }
 
 int	main(void)
@@ -28,8 +58,10 @@ int	main(void)
 	mlx_win = mlx_new_window(mlx, 500, 500, "Hello world!");
 	img.img = mlx_new_image(mlx, 500, 500);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	my_mlx_pixel_put(&img, 10, 10, 0x00FF0000);
+	put_horizontal_line(&img, 10, 10, 100);
+	put_vertical_line(&img, 10, 10, 100);
+	put_horizontal_line(&img, 10, 110, 100);
+	put_vertical_line(&img, 110, 10, 100);
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
-
